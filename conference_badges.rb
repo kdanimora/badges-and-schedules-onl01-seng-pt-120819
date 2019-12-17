80c0708d129bb4c25ce589ae14d83c948af2527d
Karen Daniela <kdaniimora@gmail.com>
2019-12-17 18:17:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,8 @@
 # Write your code here.
 def badge_maker(name)
-puts = "Hello, my name is #{name}." 
-end
+puts "Hello, my name is #{name}." end
 def batch_badge_creator(array)
   array.each do |name|
-  "Hello, my name is #{name}"
+  "Hello, my name is #{name}."
 end
 end 
