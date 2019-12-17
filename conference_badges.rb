ef954fd7f5599b4919e7785822de04a2234e50bc
Karen Daniela <kdaniimora@gmail.com>
2019-12-17 18:08:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,5 @@
 # Write your code here.
 def badge_maker (Name)
 puts = "Hello, my name is #{name}" 
-batch_badge_creator [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
-batch_badge_creator.each do |speaker|
-  assign_rooms [1, 2, 3, 4, 5, 6, 7]
-puts = "Hello #{batch_badge_creator}! You'll be assigned to room #{assign_rooms}!
-
-printer 
-puts "{badge_maker} and assign_rooms" 
 end
+def badge_maker
