96ae42cb5fa152c8d46ec0d359262f2b5ccfdea6
Karen Daniela <kdaniimora@gmail.com>
2019-12-17 18:36:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,4 +10,22 @@ def batch_badge_creator(array)
 end
 
 return speakers
+def assign_rooms(speakers)
+list_of_messages = []
+rooms = [1,2,3,4,5,6,7]
+  i = 0 
+  while i < speakers.length 
+    list_of_messages << "Hello, #{speakers[i]}! You'll be assigned to room #{rooms[i]}!"
+    i += 1
+  end 
+list_of_messages
+end 
+def printer(att) 
+  batch_badge_creator(att).each do |s|
+    puts s
+  end 
+  assign_rooms(att).each do |s|
+    puts s
+  end 
+
 end 
