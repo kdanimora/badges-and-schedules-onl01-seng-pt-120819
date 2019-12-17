8bc735fe50763044447ccceedaa064795a004855
Karen Daniela <kdaniimora@gmail.com>
2019-12-17 18:17:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ def badge_maker(name)
 puts "Hello, my name is #{name}." end
 def batch_badge_creator(array)
   array.each do |name|
-  "Hello, my name is #{name}."
+ puts "Hello, my name is #{name}."
 end
 end 
